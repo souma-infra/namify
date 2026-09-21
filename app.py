2ae33c3ada134cb4e138290f45eccac6fdc0c515
soumaditya <soumaditya.dey@nevaehtech.com>
2026-09-21 11:16:21 +0530

    third commit : tweaked the entry point code to break the pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return "Hello from Jenkins CI/CD pipeline!"
+        return "Goodbye from Jenkins!"
 
 if __name__ == '__main__':
             app.run(host='0.0.0.0', port=5000)
